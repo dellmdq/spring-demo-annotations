22141522e758a1314acbad0e98fdc872a454ee4d
dellmdq <erikdell@gmail.com>
2021-08-13 15:50:33 -0300

    BeanScopes sections. @Scope singleton and prototype

--- src/com/luv2code/springdemo/HappyFortuneService.java
+++ src/com/luv2code/springdemo/HappyFortuneService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 @Component//so springs scans and registers this class
 public class HappyFortuneService implements FortuneService {
 
+	
 	@Override
 	public String getFortune() {
 		return "Today is your lucky day!";
--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -2,9 +2,11 @@ package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component//("thatSillyCoach") to use default bean //so spring detect this class when component scanning starts. and then asigns the bean to this class.
+@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	@Autowired
